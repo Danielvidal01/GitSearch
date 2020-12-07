09da49bdb425a96c986a03fbb1a7a0039dfd472d
Daniel Vidal <danielvidalDev@outlook.com>
2020-12-07 10:20:27 -0300

    Updates

---
+++ precache-manifest.1ae3c5e498a9a652e55889cb68f624be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1e058670250db0aa6922ebaf16f222",
+    "url": "/GitSearch/index.html"
+  },
+  {
+    "revision": "7d3ea59e15cc310abd45",
+    "url": "/GitSearch/static/css/main.91a181fd.chunk.css"
+  },
+  {
+    "revision": "060cd1e1b85e833f8f19",
+    "url": "/GitSearch/static/js/2.705a198b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/GitSearch/static/js/2.705a198b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3ea59e15cc310abd45",
+    "url": "/GitSearch/static/js/main.1b22c1c4.chunk.js"
+  },
+  {
+    "revision": "c39603673173dd547ac3",
+    "url": "/GitSearch/static/js/runtime-main.5d056018.js"
+  }
+]);
